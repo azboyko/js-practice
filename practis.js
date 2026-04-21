db5b1b73183284b996181d8f05b420fc9334b020
Anastasiia <120135914+azboyko@users.noreply.github.com>
2026-04-21 18:39:15 +0000

    add new

--- practis.js
+++ practis.js
@@ -48,3 +48,61 @@ function lovefunc(flower1, flower2) {
   }
 }
 
+// Convert a string to an array
+
+
+function stringToArray(string){
+  return string.split(' ');
+}
+
+// DNA to RNA Conversion
+
+function DNAtoRNA(dna) {
+  return dna.replace(/T/g, 'U');
+}
+
+// Double Integer    https://www.codewars.com/kata/53ee5429ba190077850011d4/train/javascript
+function doubleInteger(i) {
+  i = i*2
+  return i;
+}
+
+// Twice as old    https://www.codewars.com/kata/5b853229cfde412a470000d0/train/javascript
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+  let years = 0;
+
+  while (
+    dadYearsOld + years !== 2 * (sonYearsOld + years) &&
+    dadYearsOld - years !== 2 * (sonYearsOld - years)
+  ) {
+    years++;
+  }
+
+  return years;
+}
+
+// Return n-th even number    https://www.codewars.com/kata/5933a1f8552bc2750a0000ed/train/javascript
+function nthEven(n){
+  return (n - 1) * 2;
+}
+
+// What's the real floor     https://www.codewars.com/kata/574b3b1599d8f897470018f6/train/javascriptfunction getRealFloor(n) {
+  if (n <= 0) return n;
+  if (n < 13) return n - 1;
+  return n - 2;
+}
+
+
+// Clock    https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a/train/javascript
+function past(h, m, s) {
+  return h * 3600000 + m * 60000 + s * 1000;
+}
+
+// Is n divisible by x and y    https://www.codewars.com/kata/5545f109004975ea66000086/train/javascript
+function isDivisible(n, x, y) {
+  if (n % x === 0 && n % y === 0){
+    return true;
+  } else {
+    return false;
+  }
+}
