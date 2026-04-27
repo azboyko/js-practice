e5bca67a410db8c62c86e1d9c8ee48f233f5f59d
Anastasiia <120135914+azboyko@users.noreply.github.com>
2026-04-27 08:19:30 +0000

    .

--- practice_2-3.js
+++ practice_2-3.js
@@ -48,56 +48,6 @@ function min(arr, toReturn) {
 
 
 
-// Double Integer    https://www.codewars.com/kata/53ee5429ba190077850011d4/train/javascript
-function doubleInteger(i) {
-  i = i * 2
-  return i;
-}
-
-// Twice as old    https://www.codewars.com/kata/5b853229cfde412a470000d0/train/javascript
-function twiceAsOld(dadYearsOld, sonYearsOld) {
-  let years = 0;
-
-  while (
-    dadYearsOld + years !== 2 * (sonYearsOld + years) &&
-    dadYearsOld - years !== 2 * (sonYearsOld - years)
-  ) {
-    years++;
-  }
-
-  return years;
-}
-
-// Return n-th even number    https://www.codewars.com/kata/5933a1f8552bc2750a0000ed/train/javascript
-function nthEven(n) {
-  return (n - 1) * 2;
-}
-
-// What's the real floor     https://www.codewars.com/kata/574b3b1599d8f897470018f6/train/javascriptfunction getRealFloor(n) {
-function getRealFloor(n) {
-  if (n <= 0) {
-    return n;
-  } if (n < 13) {
-    return n - 1;
-  } else {
-    return n - 2;
-  }
-}
-
-// Clock    https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a/train/javascript
-function past(h, m, s) {
-  return h * 3600000 + m * 60000 + s * 1000;
-}
-
-// Is n divisible by x and y    https://www.codewars.com/kata/5545f109004975ea66000086/train/javascript
-function isDivisible(n, x, y) {
-  if (n % x === 0 && n % y === 0) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
 
 
 
@@ -150,4 +100,31 @@ function buildFun(n){
 
 
 // https://www.codewars.com/kata/fun-with-es6-classes-number-2-animals-and-inheritance/train/javascript
-// 
+
+
+class Shark extends Animal {
+  constructor(name, age, status) {
+    super(name, age, 0, "shark", status);
+  }
+}
+
+class Cat extends Animal {
+  constructor(name, age, status) {
+    super(name, age, 4, "cat", status);
+  }
+
+  introduce() {
+    return super.introduce() + "  Meow meow!";
+  }
+}
+
+class Dog extends Animal {
+  constructor(name, age, status, master) {
+    super(name, age, 4, "dog", status);
+    this.master = master;
+  }
+
+  greetMaster() {
+    return `Hello ${this.master}`;
+  }
+}
